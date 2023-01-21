fb640db7b0d874093f81d23e5abf856a3a69d204
zcross13 <zalicross24@gmail.com>
2023-01-20 20:02:42 -0600

    finish sei cafe

--- server.js
+++ server.js
@@ -1,49 +1,98 @@
+// /server.js
 require('dotenv').config()
-// Always require and configure near the top
 require('./config/database');
-// Connects Database 
 const express = require('express')
-const path = require('path') //comes with node.js 
-const favicon = require('serve-favicon') //find with npm 
-const logger = require('morgan') // morgan isnt the only logger that exist 
-const PORT= process.env.PORT || 3001
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const PORT = process.env.PORT || 3001
 
+const app = express()
 
-const app = express() // return app object 
-
-
-
-app.use(express.json()) //gives us our req.body 
-app.use((req,res,next) => {
+app.use(express.json())// req.body
+app.use((req, res, next) => {
     res.locals.data = {}
     next()
-    //allows us to use locals in the routes
 })
-app.use(logger('dev')) // helps us debugged 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico'))) //_dirname is my directory/build folder/file name favicon.ico 
+app.use(logger('dev'))
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico' )))
 app.use(express.static(path.join(__dirname, 'build')))
 
-
-
-// Check if token and create req.user
 app.use(require('./config/checkToken'))
-
 /*
-app.use('/api', routes) <===== Finish code once you got it 
+app.use('/api', routes) <====== Finish code once you got it
 */
-
-// Put API routes here, before the "catch all" route
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/fruits', require('./routes/api/fruits'))
 
-app.get('/api/test', (req,res) => {
+const ensureLoggedIn = require('./config/ensureLoggedIn')
+app.use('/api/items', ensureLoggedIn, require('./routes/api/items'))
+app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'))
+
+app.get('/api/test', (req, res) => {
     res.json({'eureka': 'you have found it'})
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, 'build', 'index.html'))
-}) // catch all files 
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
-app.listen(PORT,  () => {
+app.listen(PORT, () => {
     console.log(`I am listening on ${PORT}`)
 })
+
+
+// require('dotenv').config()
+// // Always require and configure near the top
+// require('./config/database');
+// // Connects Database 
+// const express = require('express')
+// const path = require('path') //comes with node.js 
+// const favicon = require('serve-favicon') //find with npm 
+// const logger = require('morgan') // morgan isnt the only logger that exist 
+// const PORT= process.env.PORT || 3001
+
+
+// const app = express() // return app object 
+
+
+
+// app.use(express.json()) //gives us our req.body 
+// app.use((req,res,next) => {
+//     res.locals.data = {}
+//     next()
+//     //allows us to use locals in the routes
+// })
+// app.use(logger('dev')) // helps us debugged 
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico'))) //_dirname is my directory/build folder/file name favicon.ico 
+// app.use(express.static(path.join(__dirname, 'build')))
+
+
+
+// // Check if token and create req.user
+// app.use(require('./config/checkToken'))
+
+// /*
+// app.use('/api', routes) <===== Finish code once you got it 
+// */
+
+// // Put API routes here, before the "catch all" route
+// app.use('/api/users', require('./routes/api/users'))
+// app.use('/api/fruits', require('./routes/api/fruits'))
+
+// const ensureLoggedIn = require('./config/ensureLoggedIn')
+// app.use('/api/items', ensureLoggedIn, require('./routes/api/items'))
+// app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'))
+
+
+// app.get('/api/test', (req,res) => {
+//     res.json({'eureka': 'you have found it'})
+// })
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'))
+// }) // catch all files 
+
+// app.listen(PORT,  () => {
+//     console.log(`I am listening on ${PORT}`)
+// })
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css'
-import App from './pages/App.js/App';
+import App from './pages/App/App';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
